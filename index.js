28a2cc53a95577feac3e33f4ab5603fe1837d715
Vera Khremli <lovet.post@gmail.com>
2024-11-22 22:30:25 +0300

    feat: add task 02 code

--- index.js
+++ index.js
@@ -1,13 +1,34 @@
 const http = require('http');
-        const hostname = '127.0.0.1';
+const hostname = '127.0.0.1';
 const port = 3000;
 
-const server = http.createServer((req,res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello World');
+const server = http.createServer((req, res) => {
+    parseBodyJson(req, (err, payload) => {
+        const c = { c: payload.a + payload.b }
+
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.end(JSON.stringify(c));
+    })
 })
 
 server.listen(port, hostname, () => {
-    console.log(`Server running at htttp://${hostname}:${port}`);
+    console.log(`Server running at http://${hostname}:${port}`);
 });
+
+function parseBodyJson(req, cb) {
+    let body = [];
+    
+    req.on('data', function (chunk) {
+        body.push(chunk);
+        // console.log(body);
+    }).on('end', function () {
+        // console.log(body);
+        // console.log(Buffer.concat(body));
+        body = Buffer.concat(body).toString();
+        // console.log(body);
+        let params = JSON.parse(body);
+        // console.log(params);
+        cb(null, params)
+    })
+}
